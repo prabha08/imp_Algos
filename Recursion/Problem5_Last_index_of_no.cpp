3d65652cf74ac25d398c40e87fdfca557ee811fa
Prabha Pant <prabhapant08@gmail.com>
2020-05-14 20:58:12 +0530

    Create Problem5_Last_index_of_no.cpp

---
+++ Recursion/Problem5_Last_index_of_no.cpp
@@ -0,0 +1,20 @@
+int lastIndex(int input[], int size, int x) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+    if(size == 0) return -1;
+    
+    int i = lastIndex(input+1, size-1, x);
+    if(i >= 0) {
+        return 1+i;
+    }
+    else if(*input == x) {
+       return 0;
+    } 
+    else {
+        return -1;
+    }
+    
+}
