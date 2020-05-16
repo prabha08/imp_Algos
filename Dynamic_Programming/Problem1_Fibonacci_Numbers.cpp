0aea0aa369670c6a7bf89304c073bbd107e96ad3
Prabha Pant <prabhapant08@gmail.com>
2020-05-16 12:22:30 +0530

    Create Problem1_Fibonacci_Numbers.cpp

---
+++ Dynamic_Programming/Problem1_Fibonacci_Numbers.cpp
@@ -0,0 +1,12 @@
+/*
+Print the nth fibbonacci number 
+*/
+
+// Recurrance relation -  f(n) = f(n-1) + f(n-2)
+/* Three Approaches - 
+    1. Recurssion 
+    2. Dynamic Programming 
+    3. Iterative Soln
+ */
+ 
+ 
